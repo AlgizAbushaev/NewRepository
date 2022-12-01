be8f180a0d95f4b75e7f6c508d14f00c0ee4a88c
AlgizAbushaev <Abushaevalgiz@mail.ru>
2022-12-01 21:26:54 +0300

    TaskJDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        Util.getConnection();
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 20);
+        userService.saveUser("Petr", "Petrov", (byte) 25);
+        userService.saveUser("Serj", "Sergeev", (byte) 30);
+        userService.saveUser("Vasiliy", "Vasin", (byte) 35);
+
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+        public static final String URL = "jdbc:mysql://localhost:3306/zadacha1";
+        public static final String USER = "root";
+        public static final String PASSWORD = "root";
+
+
+        public static Connection getConnection() {
+            Connection connection = null;
+
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                connection.setAutoCommit(false);
+                System.out.println("Connection OK");
+                return connection;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Connection ERROR");
+            }
+            return connection;
+        }
 }
+
+
+
+
